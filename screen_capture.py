e14f2084d871b51966da14adff428aade047f989
JamesBear <jamesgrizzly@gmail.com>
2018-09-26 00:28:30 +0800

    add shutter sound effect

--- screen_capture.py
+++ screen_capture.py
@@ -2,11 +2,42 @@ import keyboard
 import pyautogui
 from datetime import datetime
 import os
+import pyaudio
+import wave
+import sys
 
 SAVE_DIR = 'C:\\screenshots\\'
+WAV_FILE = 'camera4.wav'
+wf = None
 
 if not os.path.isdir(SAVE_DIR):
     os.mkdir(SAVE_DIR)
+
+def play_shutter_sound():
+    CHUNK = 1024
+
+    global wf
+    if wf == None:
+        wf = wave.open(WAV_FILE, 'rb')
+    wf.setpos(0)
+    p = pyaudio.PyAudio()
+
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+
+    while data != '' and data != b'':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
 
 def screenshot_all():
     print('screenshot all')
@@ -16,8 +47,10 @@ def screenshot_all():
 
     pic.save(file_name)
     print('   saved to {}'.format(file_name))
+    play_shutter_sound()
 
-keyboard.add_hotkey('ctrl+f12', screenshot_all, args=())
+if __name__ == '__main__':
+    keyboard.add_hotkey('ctrl+f12', screenshot_all, args=())
 
-# Block forever, like `while True`.
-keyboard.wait()
+    # Block forever, like `while True`.
+    keyboard.wait()
